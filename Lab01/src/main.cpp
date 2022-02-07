faea56c26601fc3b941b85d7ac9fa416facdcfa4
mononerv <pratchaya.k.git@gmail.com>
2022-02-07 16:54:20 +0100

    Initial commit

---
+++ Lab01/src/main.cpp
@@ -0,0 +1,13 @@
+#include <array>
+#include <cstdint>
+
+#include "esp_task_wdt.h"
+#include "driver/dac.h"
+#include "soc/dac_channel.h"
+#include "esp32/rom/ets_sys.h"
+
+extern "C" auto app_main() -> void;
+
+auto app_main() -> void {
+}
+
