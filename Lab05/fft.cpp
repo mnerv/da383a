3d1244ee7a25834550da38991cc332d7195e66b0
mononerv <pratchaya.k.git@gmail.com>
2022-08-11 03:44:16 +0200

    Lab05: Remove fft.cpp in Lab05
    No FFT implementation.

--- Lab05/fft.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cstdint>
-
-auto main([[maybe_unused]]std::int32_t argc, [[maybe_unused]]char const* argv[]) -> std::int32_t {
-    std::cout << "Hello, FFT!" << "\n";
-    return 0;
-}
-
